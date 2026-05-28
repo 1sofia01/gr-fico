0c3964c68d3b4ea43ffc1fb81b12202e51b89c7d
1sofia01 <sofiaaah88@gmail.com>
2026-05-28 09:23:10 -0300

    Fix CSV read by adding index_col and separator

--- grafico.py
+++ grafico.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('eleitorado_por_zona_eleitoral.csv', index_col =)
+df = pd.read_csv('eleitorado_por_zona_eleitoral.csv', index_col = 'NUM_ZONA', sep=';')
 st.bar_chart(df)
