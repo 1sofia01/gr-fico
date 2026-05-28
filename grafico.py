17eb42252d159f2e6bf1916c57fce9b71168abcf
1sofia01 <sofiaaah88@gmail.com>
2026-05-28 10:32:51 -0300

    Fix syntax error in bar_chart function call

--- grafico.py
+++ grafico.py
@@ -2,10 +2,7 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_excel('exportacao_geral.xlsx')
+
 st.title('Exportação por Estado (UF)')
 
-st.bar_chart(
-  df,
-  x='UF do Produto'
-  y =['2021 - Valor US$ FOB', '2022 - Valor US$ FOB']
-)
+st.bar_chart(df, x='UF do Produto' y =['2021 - Valor US$ FOB', '2022 - Valor US$ FOB'])
