337254e758e02e06625a9dc4ebfcd06b134abc16
1sofia01 <sofiaaah88@gmail.com>
2026-05-28 10:36:05 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -5,4 +5,4 @@ df = pd.read_excel('exportacao_geral.xlsx')
 
 st.title('Exportação por Estado (UF)')
 
-st.bar_chart(df, x='UF do Produto' y =['2021 - Valor US$ FOB', '2022 - Valor US$ FOB'])
+st.bar_chart(df, x='UF do Produto', y=['2021 - Valor US$ FOB', '2022 - Valor US$ FOB'])
